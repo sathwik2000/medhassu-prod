3155c64cda529aa8e6603011d70be219d35af81a
sathwik2000 <34467058+sathwik2000@users.noreply.github.com>
2025-11-02 22:08:22 +0530

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
-module.exports = {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
-};
+}
+
+export default config
