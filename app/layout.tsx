cd947dff09522514f1d5112052a9ee16fc04edbb
sathwik2000 <34467058+sathwik2000@users.noreply.github.com>
2025-11-03 11:56:49 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,16 +15,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className="antialiased">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
+          {/* Fixed Navbar */}
           <Navbar />
-          <AppSidebar />
-          <main>{children}</main>
+
+          <div className="flex">
+            {/* Fixed Sidebar */}
+            <aside
+              data-slot="sidebar"
+              className="fixed top-[64px] left-0 w-64 h-[calc(100vh-64px)] border-r border-[hsl(var(--color-sidebar-border))] bg-[hsl(var(--color-sidebar))] overflow-y-auto z-40"
+            >
+              <AppSidebar />
+            </aside>
+
+            {/* Main content area */}
+            <main className="flex-1 ml-64 mt-[64px] min-h-[calc(100vh-64px)] bg-[hsl(var(--color-background))] p-6 md:p-10 transition-colors">
+              <div className="max-w-4xl mx-auto bg-[hsl(var(--color-card))] text-[hsl(var(--color-card-foreground))] rounded-xl shadow-lg border border-[hsl(var(--color-border))] p-8">
+                {children}
+              </div>
+            </main>
+          </div>
         </ThemeProvider>
       </body>
     </html>
